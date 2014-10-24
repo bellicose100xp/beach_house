080af6fa2a9b33e74d705b4da50f4cf41f4a77c4
buggy <chandan7@icloud.com>
2014-10-24 09:05:14 -0700

    home_update

--- js/app.js
+++ js/app.js
@@ -8,22 +8,30 @@
 
     app.controller("mainController", function () {
         var homes = this;
+        homes.showButtons = true;
+
+        homes.hideButtons = function(){
+          homes.showButtons = !homes.showButtons;
+        };
 
         homes.list = [
             {
                 "name": "20300 Pacific Coast Highway",
+                "pageId": "20300",
                 "ourWebsite": "http://rentalhomebyowners.com/20300pch.html",
                 "vrbo": "http://www.vrbo.com/412480",
                 "airBnB": "https://www.airbnb.com/rooms/4033105"
             },
             {
                 "name": "21010 Pacific Coast Highway",
+                "pageId": "21010",
                 "ourWebsite": "http://rentalhomebyowners.com/21010pch.html",
                 "vrbo": "http://www.vrbo.com/602697",
                 "airBnB": "https://www.airbnb.com/rooms/4033263"
             },
             {
                 "name": "26050 Pacific Coast Highway",
+                "pageId": "26050",
                 "ourWebsite": "http://rentalhomebyowners.com/20300pch.html",
                 "vrbo": "http://www.vrbo.com/403097",
                 "airBnB": "https://www.airbnb.com/rooms/4235156"
@@ -33,5 +41,20 @@
         homes.title = "Master Beach House List";
     });
 
+    app.config(function($stateProvider,$urlRouterProvider){
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home',{
+                url: '/',
+                templateUrl: '/index.html'
+            })
+            .state("20300",{
+                url: "/20300",
+                templateUrl: "/20300.html"
+            })
+
+    });
 
 }());
